7a98ef08b43b544e39b2c9a8ce7ad82acdcc521e
zyf <flzzyf076@gmail.com>
2021-03-23 18:01:37 +0800

    1

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,35 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour {
     public InventoryLayoutGroup inventoryLayoutGroup;
+    public GameObject itemPrefab;
 
     public UIMover uiMover;
 
-    void Start() {
-        //print(inventoryLayoutGroup.GetFirstItemPos());
-
-        //Vector2 pos = inventoryLayoutGroup.GetFirstItemPos();
-
-        //GameObject go = new GameObject("qwe");
-        //go.AddComponent<RectTransform>();
-        //go.transform.SetParent(FindObjectOfType<Canvas>().transform);
-
-        //go.transform.localScale = Vector3.one;
-        //go.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1);
-        //go.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1);
-        //go.GetComponent<RectTransform>().anchoredPosition = pos;
-        //go.GetComponent<RectTransform>().sizeDelta = inventoryLayoutGroup.GetActualSize();
-    }
-
     private void Update() {
         if (Input.GetKeyDown("1")) {
-            uiMover.UIMover_Show();
+            //uiMover.target.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1);
+            //uiMover.target.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1);
+            //uiMover.target.GetComponent<RectTransform>().sizeDelta = inventoryLayoutGroup.GetActualSize();
+            //uiMover.target.GetComponent<RectTransform>().anchoredPosition = inventoryLayoutGroup.GetFirstItemPos();
+            uiMover.Move(0, 1);
+
+            //Instantiate(itemPrefab, inventoryLayoutGroup.transform);
+
         }
         if (Input.GetKeyDown("2")) {
-            uiMover.UIMover_Hide();
+            uiMover.Move(1, 0);
         }
+
+        //QWE();
+
     }
 
+    GameObject go;
+    void QWE() {
+        if (go) {
+            Destroy(go);
+        }
+
+        Vector2 pos = inventoryLayoutGroup.GetFirstItemPos();
+
+        go = new GameObject("qwe");
+        go.AddComponent<RectTransform>();
+        go.AddComponent<Image>().color = Color.black;
+        go.transform.SetParent(FindObjectOfType<Canvas>().transform);
+
+        go.transform.localScale = Vector3.one;
+        go.GetComponent<RectTransform>().anchorMin = new Vector2(0, 1);
+        go.GetComponent<RectTransform>().anchorMax = new Vector2(0, 1);
+        go.GetComponent<RectTransform>().anchoredPosition = pos;
+        go.GetComponent<RectTransform>().sizeDelta = inventoryLayoutGroup.GetActualSize();
+    }
 }
--- Assets/Scripts/UIMover.cs
+++ Assets/Scripts/UIMover.cs
@@ -6,9 +6,8 @@ using System;
 
 public class UIMover : MonoBehaviour {
     [Header("初始位置")]
-    public RectTransform pos1;
-    [Header("目标位置")]
-    public RectTransform pos2;
+    public RectTransform[] rectTrasnfroms;
+    public Rect rects;
 
     [Header("移动物体")]
     public RectTransform target;
@@ -23,21 +22,17 @@ public class UIMover : MonoBehaviour {
     [Header("位置变化_隐藏")]
     public UXMovement movement_Pos_Hide;
 
-    public void UIMover_Show() {
-        target.sizeDelta = pos1.sizeDelta;
-        target.position = pos1.position;
+    public void Move(Rect from, Rect to, Action onComplete = null) {
+        target.sizeDelta = from.size;
+        target.position = from.position;
 
-        target.DOSizeDelta(pos2.sizeDelta, movement_Size_Show.duration).SetEase(movement_Size_Show.curve);
-        target.DOMove(pos2.position, movement_Pos_Show.duration).SetEase(movement_Pos_Show.curve);
-    }
-
-    public void UIMover_Hide(Action onComplete = null) {
-        target.sizeDelta = pos2.sizeDelta;
-        target.position = pos2.position;
-
-        target.DOSizeDelta(pos1.sizeDelta, movement_Size_Hide.duration).SetEase(movement_Size_Hide.curve);
-        target.DOMove(pos1.position, movement_Pos_Hide.duration).SetEase(movement_Pos_Hide.curve).OnComplete(() => {
+        target.DOSizeDelta(to.size, movement_Size_Show.duration).SetEase(movement_Size_Show.curve);
+        target.DOMove(to.position, movement_Pos_Show.duration).SetEase(movement_Pos_Show.curve).OnComplete(() => {
             onComplete?.Invoke();
         });
+    }
+
+    public void Move(int fromIndex, int toIndex, Action onComplete = null) {
+        Move(rectTrasnfroms[fromIndex].rect, rectTrasnfroms[toIndex].rect, onComplete);
     }
 }
